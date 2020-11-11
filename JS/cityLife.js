b12b78fe22773e4ff203eb50778b7c72cbe9a4c9
jiwon <jidole041214@naver.com>
2020-11-11 18:48:19 +0900

    [ADD] 로또샵 기능 추가

--- JS/cityLife.js
+++ JS/cityLife.js
@@ -2,10 +2,13 @@
 let i = 1;
 let j = 0;
 char.style.transition = "0s";
+let charVal = 1;
 
 $(document).keydown(function(event) {
-    char.src = "./../IMG/캐릭터2.png";
+    char.src = "./../IMG/캐릭터" + charVal + ".png";
     if (event.keyCode == '37') {
+        charVal = 2;
+        char.src = "./../IMG/캐릭터2.png"
         if(char.style.marginLeft.split("px")[0]==0){
             return;
         }else{ // 끝에서 멈추게
@@ -14,6 +17,7 @@ $(document).keydown(function(event) {
         i--;
     } // 왼쪽으로 움직이기
     else if (event.keyCode == '39') {
+        charVal = 1; 
         char.src = "./../IMG/캐릭터1.png";
         if(char.style.marginLeft.split("px")[0]>=3290){
             return;
@@ -40,6 +44,10 @@ $(document).keydown(function(event) {
   let arrow2 = document.getElementById('arrow2');
   let alertVal3 = document.getElementById('alert3'); 
   let arrow3 = document.getElementById('arrow3');
+  let alertVal4 = document.getElementById('alert4'); 
+  let arrow4 = document.getElementById('arrow4');
+  let alertVal5 = document.getElementById('alert5'); 
+  let arrow5 = document.getElementById('arrow5');
   window.onload = function(){
       setInterval(() => {
         console.log(char.style.marginLeft)
@@ -48,7 +56,7 @@ $(document).keydown(function(event) {
             arrow.style.display = 'block';
             $(document).keydown(function(event) {
                 if(event.keyCode == '38'){
-                    location.href = "https://www.dsm-dms.com/";
+                    location.href = "./../HTML/conveniencePage.html";
                     val = !val;
                 }
               }); // 캐릭터 움직이는 거 구현 
@@ -57,12 +65,38 @@ $(document).keydown(function(event) {
             alertVal.style.display = "none";
             arrow.style.display = 'none';
         }
+        if(char.style.marginLeft == '1120px'){
+            alertVal5.style.display = "flex";
+            arrow5.style.display = 'block'
+             $(document).keydown(function(event) {
+                if(event.keyCode == '38'){
+                    location.href = "./../HTML/lottoShop.html";
+                }
+              }); // 캐릭터 움직이는 거 구현 
+        }        
+        else{
+            alertVal5.style.display = "none";
+            arrow5.style.display = 'none'
+        }
+        if(char.style.marginLeft == '1470px'){
+            alertVal4.style.display = "flex";
+            arrow4.style.display = 'block'
+             $(document).keydown(function(event) {
+                if(event.keyCode == '38'){
+                    location.href = "./../HTML/mainMenu.html";
+                }
+              }); // 캐릭터 움직이는 거 구현 
+        }        
+        else{
+            alertVal4.style.display = "none";
+            arrow4.style.display = 'none'
+        }
         if(char.style.marginLeft == '1960px'){
             alertVal2.style.display = "flex";
             arrow2.style.display = 'block'
              $(document).keydown(function(event) {
                 if(event.keyCode == '38'){
-                    location.href = "https://www.dsm-dms.com/";
+                    location.href = "./../HTML/artGallery.html";
                 }
               }); // 캐릭터 움직이는 거 구현 
         }        
@@ -75,7 +109,7 @@ $(document).keydown(function(event) {
             arrow3.style.display = 'block'
              $(document).keydown(function(event) {
                 if(event.keyCode == '38'){
-                    location.href = "https://www.dsm-dms.com/";
+                    location.href = "./../HTML/loginPage.html";
                 }
               }); // 캐릭터 움직이는 거 구현 
         }        
